f161c46b937845557b1da1d6d6f37841453c7c0b
陈焱 <alberteinstein007@126.com>
2020-12-05 20:45:10 +0800

    refator: refator index.ts template

--- generators/app/templates/src/index.ts
+++ generators/app/templates/src/index.ts
@@ -1,6 +1,22 @@
-import greeting from './components/other';
-
-function myplugins(params:string) : void {
-    console.log(greeting() + params);
+type Cms = {
+    [k in 'abbreviation' | 'params' | 'description']: string
 }
-export default myplugins;
+type CmdDesctions = {
+    name: string
+} & Partial<Cms>;
+
+//write your cmd configrations here:
+const CmdDesctions:CmdDesctions = {
+    name: '', //command name 
+    abbreviation: '', //first params abbreviation
+    params: '' // fisrt params name
+}
+const happy = (register: any) => {
+    console.log("happy plugins loaded...");
+    register(CmdDesctions, (log: string) => {
+        //write or import modules here
+        console.log(log);
+    }, "log your logger");
+}
+
+module.exports = happy;
